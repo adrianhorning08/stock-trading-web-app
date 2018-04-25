3df210803c131321276feaa6857f273d4472ddc4
Adrian Horning <m.adrian.horning@gmail.com>
2018-04-24 17:03:45 -0700

    Buy shares form

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -2,15 +2,50 @@ import React from 'react';
 import StockItem from '../stocks/stock_items';
 import { fetchCurrPrice } from '../../util/stocks_api_util';
 
+class BuyStockForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      amount: undefined
+    };
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+  }
+
+  update() {
+    return e => {
+      this.setState({amount: e.target.value.replace(/\D/,'')});
+    };
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <input
+          type="number"
+          value={this.state.amount}
+          placeholder="How many shares?"
+          onChange={this.update}
+          />
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       stockName: '',
-      stockPrice: null
+      stockPrice: null,
+      showForm: false
     };
     this.searchForStock = this.searchForStock.bind(this);
     this.buyStockButton = this.buyStockButton.bind(this);
+    this.handleBuyStockSubmit = this.handleBuyStockSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -32,9 +67,13 @@ class Dashboard extends React.Component {
     });
   }
 
+  handleBuyStockSubmit(e) {
+    this.setState({showForm: !this.state.showForm});
+  }
+
   buyStockButton() {
     if (this.state.stockPrice) {
-      return <button>Buy</button>;
+      return <button onClick={this.handleBuyStockSubmit}>Buy</button>;
     } else {
       return null;
     }
@@ -62,12 +101,13 @@ class Dashboard extends React.Component {
         <input
           type='text'
           value={this.state.stockName}
-          onChange={this.update('stockName')}
+          onChange={this.update()}
           placeholder="Type in ticker symbol"
           />
         <button onClick={this.searchForStock}>Check stock</button>
         <h2>Current Price</h2>
         {this.state.stockPrice}{this.buyStockButton()}
+        {this.state.showForm && < BuyStockForm / >}
         <h1>My Stocks</h1>
         {stockList}
       </div>
